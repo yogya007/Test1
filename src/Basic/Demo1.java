fa45aa1489eb5d89796d5306c404cfe05513920f
Aakash Koli <Aakash Koli@LAPTOP-OAN6BTMA>
2023-01-28 17:07:33 +0530

    First commit

---
+++ src/Basic/Demo1.java
@@ -0,0 +1,27 @@
+package Basic;
+
+public class Demo1
+{
+	public static void main(String[] args) 
+	 {
+		
+		 
+		  System.out.println("Good Morning");
+		 
+		 
+		 
+		 
+		 
+	 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
